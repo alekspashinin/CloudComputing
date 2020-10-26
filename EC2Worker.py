239e44bf9254a32f90253a03918c5c0e2af37751
Aleks Pashinin <alekspashinin@gmail.com>
2020-10-26 11:31:59 +0100

    Update EC2Worker and SenderS3

--- EC2Worker.py
+++ EC2Worker.py
@@ -13,11 +13,13 @@ import sys
 import time
 import boto3
 import statistics
-import SenderS3
+import SenderS3 as SS3
+
 
 def mathCalculate(valuesList):
-    answer = [min(valuesList), max(valuesList), sum(valuesList)/len(valuesList), statistics.median(valuesList)]
+    answer = [min(valuesList), max(valuesList), sum(valuesList) / len(valuesList), statistics.median(valuesList)]
     return (answer)
+
 
 def resetMessages(queue):
     messages = queue.receive_messages()
@@ -26,6 +28,7 @@ def resetMessages(queue):
         queue.purge()
         print('Number of messages received: ', len(messages))
 
+
 def stringToList(entryList):
     workString = entryList
     workList = workString.split()
@@ -33,40 +36,45 @@ def stringToList(entryList):
     resultList = list(mapObject)
     return resultList
 
+
 def listToString(entryList):
     # initialize an empty string
     resultList = " "
     # return string
     return (resultList.join(map(str, entryList)))
 
+
 # Starting Project
 # TEST PART
-#s3 = boto3.resource('s3')
-#for bucket in s3.buckets.all():
+# s3 = boto3.resource('s3')
+# for bucket in s3.buckets.all():
 #    print(bucket.name)
-#data = open('clouds.jpg', 'rb')
-#s3.Bucket('mybucketnumber1').put_object(Key='clouds.jpg', Body=data)
+# data = open('clouds.jpg', 'rb')
+# s3.Bucket('mybucketnumber1').put_object(Key='clouds.jpg', Body=data)
 
 # Create the queue. This returns an SQS.Queue instance
 sqs = boto3.resource('sqs')
 s3 = boto3.resource('s3')
+bucket = s3.Bucket('mybucketnumber1')
 key = 'logs1.txt'
+print('Received: ')
 queue = sqs.create_queue(QueueName='CloudComputingProject7')
-#resetMessages(queue)
+# resetMessages(queue)
 while True:
     messages = queue.receive_messages()
-    print('Number of messages received: ',len(messages))
+    print('Number of messages received: ', len(messages))
     for message in messages:
-            print('Received: ', message.body)
-            resultList = stringToList(message.body)
-            print ('Result of Calculation: ', mathCalculate(resultList))
-            sendList =listToString(mathCalculate(resultList))
-            print('Send Message: ', sendList)
-            message.delete()
-            queue = sqs.get_queue_by_name(QueueName='CloudComputingProject8')
-            # Create a new message
-            response = queue.send_message(MessageBody=sendList)
-            SenderS3.write_logs(sendList, s3, key)
-            sys.exit("Stop code")
+        print('Received: ', message.body)
+        resultList = stringToList(message.body)
+        print('Result of Calculation: ', mathCalculate(resultList))
+        sendList = listToString(mathCalculate(resultList))
+        print('Send Message: ', sendList)
+        message.delete()
+        queue = sqs.get_queue_by_name(QueueName='CloudComputingProject8')
+        # Create a new message
+        response = queue.send_message(MessageBody=sendList)
+        SS3.writefile(sendList, bucket, key)
+        sys.exit("Stop code")
 
-
+if __name__ == "__main__":
+    print("Hello World!")
--- SenderS3.py
+++ SenderS3.py
@@ -10,13 +10,11 @@ def write_logs(body, bucket, key):
     s3.put_object(Body=body, Bucket=bucket, Key=key)
 
 
-log = logging.getLogger("S3 Logs")
-log_stringio = io.StringIO()
-handler = logging.StreamHandler(log_stringio)
-log.addHandler(handler)
-
-atexit.register(write_logs, body=log_stringio.getvalue(), bucket="testName", key="testKey")
-
-log.info("S3 Test")
-
-quit()
+def writefile(body, bucket, key):
+    log = logging.getLogger("S3 Logs")
+    log_stringio = io.StringIO()
+    handler = logging.StreamHandler(log_stringio)
+    log.addHandler(handler)
+    atexit.register(write_logs, body=log_stringio.getvalue(), bucket="mybucketnumber1", key="testKey")
+    log.info("S3 Test")
+    quit()
