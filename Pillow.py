9300042d48d9a1baa506019a305394cb48a9fdee
hamed.rahimi <41012070+HamedRahimi@users.noreply.github.com>
2020-10-27 11:03:52 +0100

    Create Pillow.py
    Image proccessing

---
+++ Pillow.py
@@ -0,0 +1,7 @@
+from PIL import Image, ImageFilter, ImageEnhance 
+#Read image
+im = Image.open( 'image.jpg' )
+#Display image
+im.show()
+enh = ImageEnhance.Contrast(im)
+enh.enhance(1.8).show("30% more contrast")
