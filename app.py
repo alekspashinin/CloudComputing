ffb12190626b33f0cc15bd02bdd1cb843a0e4aa5
Aleks Pashinin <alekspashinin@gmail.com>
2020-10-28 23:16:32 +0100

    Almost Final Commit of Web App

--- app.py
+++ app.py
@@ -35,38 +35,6 @@ def stringToList(entryList):
     resultList = list(mapObject)
     return resultList
 
-class Compute2(Thread):
-    def __init__(self, request):
-        Thread.__init__(self)
-        self.request = request
-
-    def download(self):
-        s3 = boto3.client('s3')
-        s3.download_file('your_bucket',self,'/Users/Aleksei Pashinin/Desktop'+self)
-        data="Downloaded! Saved in /Users/Aleksei Pashinin/Desktop"
-        return render_template('part2.html', message=data)
-
-class Compute1(Thread):
-    def __init__(self, request):
-        Thread.__init__(self)
-        self.request = request
-
-    def receive(self):
-        messages = queue.receive_messages()
-        while True:
-            for message in messages:
-                data = stringToList(message.body)
-                message.delete()
-            return (data)
-
-    def run(self):
-        sqs = boto3.resource('sqs')
-        queue = sqs.create_queue(QueueName='CloudComputingProject8')
-        data = receive()
-        thread_b = Compute2(request.__copy__(data))
-        thread_b.start()
-        return render_template('part2.html', message=data)
-
 
 @app.route('/')
 def main():
@@ -76,31 +44,63 @@ def main():
 def part1():
     return render_template('part1.html')
 
+@app.route('/part1wait', methods=['POST'])
+def part1wait():
+    print("START RUN")
+    sqs = boto3.resource('sqs')
+    queue = sqs.create_queue(QueueName='CloudComputingProject8')
+    print("START RECEIVE")
+    messages = queue.receive_messages()
+    while True:
+        for message in messages:
+            tempos = message.body
+            data = list(tempos.split(" "))
+            message.delete()
+            finalMessage = "<<<<< Received >>>>> Min: {}, Max: {}, Average: {}, Median: {}".format(data[0], data[1], data[2], data[3])
+        return render_template('answerpart1.html', message=finalMessage)
+    return render_template('part2wait.html', message="Wait answer. Answers for this moment: 0 ")
+
+@app.route('/part2wait', methods=['POST'])
+def part2wait():
+    print("START RUN")
+    sqs = boto3.resource('sqs')
+    queue = sqs.get_queue_by_name(QueueName='inbox1')
+    print("START RECEIVE")
+    messages = queue.receive_messages()
+    while True:
+        for message in messages:
+            tempos = message.body
+            message.delete()
+            s3 = boto3.client('s3')
+            s3.download_file('mybucketnumber1', tempos, tempos)
+            finalMessage = "<<<<< Received >>>>> Modified file name: {}, Image downloaded".format(tempos)
+        return render_template('answerpart2.html', message=finalMessage)
+    return render_template('part2wait.html', message="Wait answer. Answers for this moment: 0 ")
+
 @app.route('/part2')
 def part2():
     return render_template('part2.html')
 
-@app.route("/part2", methods=["POST"])
-def receive():
-    messages = queue.receive_messages()
-    while True:
-            for message in messages:
-                data = stringToList(message.body)
-                message.delete()
-            return(data)
+@app.route('/answerpart1')
+def answerpart1():
+    return render_template('answerpart1.html')
 
+@app.route('/answerpart2')
+def answerpart2():
+    return render_template('answerpart2.html')
+
+@app.route("/part2", methods=["POST"])
 def create():
     # check whether an input field with name 'user_file' exist
     if 'user_file' not in request.files:
         return render_template('part2.html', message='No user_file key in request.files')
 
-    if 'keyfile' not in request.form:
-        return render_template('part2.html', message='No keyfile in request.form')
-
     # after confirm 'user_file' exist, get the file from input
     file = request.files['user_file']
     keyfile = request.form['keyfile']
     print(keyfile)
+    if keyfile is None:
+        return render_template('part2.html', message='No keyfile in request.form')
     # check whether a file is selected
     if file.filename == '':
         return render_template('part2.html', message='No selected file')
@@ -113,14 +113,19 @@ def create():
                 filename = secure_filename(file.filename)
                 file.save(filename)
                 print(keyfile)
+                s3 = boto3.client('s3')
                 s3.upload_file(
                     Bucket='mybucketnumber1',
                     Filename=filename,
                     Key=keyfile
                 )
-                thread_a = Compute1(request.__copy__())
-                thread_a.start()
-                return render_template('part2.html', message='Success upload!!! Wait for key...')
+                sqs = boto3.resource('sqs')
+                message = "{} mybucketnumber1".format(keyfile)
+                queue = sqs.get_queue_by_name(QueueName='outbox1')
+                response = queue.send_message(MessageBody=message)
+                s3 = boto3.resource('s3')
+                queue = sqs.create_queue(QueueName='inbox1')
+                return render_template('part2wait.html', message='Success upload!!! Wait for key...')
 
         # upload failed, redirect to upload page
         else:
@@ -131,7 +136,7 @@ def create():
         return render_template('part2.html', message="File type not accepted,please try again")
 
 @app.route('/send', methods=['POST'])
-def send(sum=sum):
+def send():
     if request.method == 'POST':
         sqs = boto3.resource('sqs')
         StringFromUser = request.form['num1']
@@ -139,72 +144,7 @@ def send(sum=sum):
 
         # Create a new message
         response = queue.send_message(MessageBody=StringFromUser)
-        return render_template('app.html', sum="Wait answer. Answers for this moment: 0 ")
-
-@app.route('/result', methods=['POST'])
-def result(sum=sum):
-    if request.method == 'POST':
-         sqs = boto3.resource('sqs')
-         StringFromUser = request.form['num1']
-         queue = sqs.get_queue_by_name(QueueName='CloudComputingProject7')
-
-         # Create a new message
-         response = queue.send_message(MessageBody=StringFromUser)
-         return render_template('app.html', sum="Wait answer. Answers for this moment: 0 ")
-
-@app.route('/flash/<message>')
-def flash(message):
-        return render_template('flash.html', msg=message)
-
-        """queue = sqs.create_queue(QueueName='CloudComputingProject8')
-        time.sleep(3)
-        while True:
-            messages = queue.receive_messages()
-            print('Wait answer. Answers for this moment: ', len(messages))
-            print('Received >>>', messages)
-            for message in messages:
-                print('Received >>>', message.body)
-                resultList = stringToList(message.body)
-                message.delete()
-                print('Received >>> Min: ', resultList[0], 'Max: ', resultList[1], 'Average: ', resultList[2],
-                      'Median: ', resultList[3])
-                print('Work done')
-
-        num2 = request.form['num2']
-        operation = request.form['operation']
-
-        if operation == 'add':
-            sum = float(num1) + float(num2)
-            return render_template('app.html', sum=sum)
-
-        elif operation == 'subtract':
-            sum = float(num1) - float(num2)
-            return render_template('app.html', sum=sum)
-
-        elif operation == 'multiply':
-            sum = float(num1) * float(num2)
-            return render_template('app.html', sum=sum)
-
-        elif operation == 'divide':
-            sum = float(num1) / float(num2)
-            return render_template('app.html', sum=sum)
-        else:
-            return render_template('app.html')
-            
-            
-            
-            
-                                                    <label for="Operation">Operation</label>
-                                        <select class="u-full-width" name="operation">
-                                          <option value="add">Add</option>
-                                          <option value="subtract">Subtract</option>
-                                          <option value="multiply">Multiply</option>
-                                          <option value="divide">Divide</option>
-                                        </select>
-                                        
-                                        
-                                        
-                                        """
+        return render_template('part1wait.html', message="Wait answer. Answers for this moment: 0 ")
 
 
 if __name__ == ' __main__':
